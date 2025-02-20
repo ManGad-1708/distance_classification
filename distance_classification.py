3f47efe95480e1e89e18203e1755737063813547
ManGad-1708 <manang1708@gmail.com>
2025-02-21 01:06:10 +0530

    Initial project setup

---
+++ distance_classification.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Placeholder dataset
+data = {
+    "Feature1": [1, 2, 3, 4, 5],
+    "Feature2": [5, 4, 3, 2, 1],
+    "Label": ["A", "B", "A", "B", "A"]
+}
+
+df = pd.DataFrame(data)
+print(df.head())
